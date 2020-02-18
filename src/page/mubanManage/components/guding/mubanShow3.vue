80afa6f07c41919ad745a2cc4123a1100c7f6302
jingyoushui <2625882457@qq.com>
2020-02-18 19:47:24 +0800

    统计得票信息和已投票查询

--- src/page/mubanManage/components/guding/mubanShow3.vue
+++ src/page/mubanManage/components/guding/mubanShow3.vue
@@ -3,17 +3,9 @@
         <div class="info_container" ref="info_container">
             <h1>拟晋升职级人选部门民主测评票（勾选）</h1>
             <el-card class="box-card" style="width: 960px;margin: 5px auto;">
-                <el-input class="textarea"
-                          type="textarea"
-                          :rows="2"
-                          placeholder="请输入说明或备注"
-                          v-model="note"
-                          style="width: 100%;font-size: 16px;margin-top: 10px;">
-                </el-input>
-            </el-card>
-            <el-card class="box-card" style="width: 960px;margin: 5px auto;">
-                <div style="width: 960px;height: 20px;margin: 0 auto">
-                    <span style="float: left">部门:<el-input v-model="department" placeholder="请输入部门" clearable style="width: 200px;"></el-input></span>
+                <div style="width: 960px;height: 20px;padding-bottom: 10px;">
+                    <span style="float: left">部门:<el-input v-model="department" placeholder="请输入部门" clearable
+                                                           style="width: 200px;"></el-input></span>
                     <span style="float:left;padding-left: 450px;">
                     <div class="block">
                     <span class="demonstration">时间:</span>
@@ -29,39 +21,85 @@
                 </span>
                 </div>
             </el-card>
-            <el-card class="box-card" style="width: 960px;margin: 1px auto;"><img style="width: 900px;height: 150px;padding-top: 10px;" :src="muban3_Img" /></el-card>
-            <el-card class="box-card" v-for="i in input_num" style="width: 960px;min-height: 60px;margin: 0px auto;">
-                <el-input
-                        v-model="namelist[i-1].xuhao"
-                        placeholder="序号"
-                        clearable
-                        style="width: 100px;padding-left:10px;float:left;" >
-                </el-input>
-                <el-input
-                        v-model="namelist[i-1].bumen"
-                        placeholder="部门"
-                        clearable
-                        style="width: 150px;padding-left:10px;float:left;" >
-                </el-input>
-                <el-input
-                        v-model="namelist[i-1].name"
-                        placeholder="姓名"
-                        clearable
-                        style="width: 150px;padding-left:10px;float:left;" >
-                </el-input>
-                <el-input
-                        v-model="namelist[i-1].now_ji"
-                        placeholder="现职级"
-                        clearable
-                        style="width: 200px;padding-left:10px;float:left;" >
-                </el-input>
-                <el-input
-                        v-model="namelist[i-1].next_ji"
-                        placeholder="拟晋升职级"
-                        clearable
-                        style="width: 200px;padding-left:10px;float:left;" >
+            <el-card class="box-card set_padding" style="width: 960px;margin: 1px auto;">
+                <div v-html="tableData3" class="table_show"></div>
+
+            </el-card>
+            <el-card class="box-card set_padding" v-for="i in input_num"
+                     style="width: 960px;min-height: 62px;margin: 0px auto;">
+                <div class="card_style">
+
+                    <el-input
+                            v-model="namelist[i-1].xuhao"
+                            placeholder="序号"
+                            style="width: 47px;padding-left:10px;float:left;padding-top: 10px;">
+                    </el-input>
+                    <div style="padding-left:7px;width: 1px;height: 60px;border-right: 1px solid black; float:left;"></div>
+                    <el-input
+                            v-model="namelist[i-1].bumen"
+                            placeholder="输入部门"
+                            size="large"
+                            style="width: 111px;float:left;padding-top:10px;">
+
+                    </el-input>
+                    <div style="padding-left:0px;width: 1px;height: 60px;border-right: 1px solid black; float:left;"></div>
+
+                    <el-input
+                            v-model="namelist[i-1].name"
+                            placeholder="输入姓名"
+                            size="large"
+                            style="width: 127px;float:left;padding-top:10px;">
+
+                    </el-input>
+                    <div style="padding-left:0px;width: 1px;height: 60px;border-right: 1px solid black; float:left;"></div>
+
+                    <el-input
+                            v-model="namelist[i-1].now_ji"
+                            placeholder="输入现职级"
+                            size="large"
+                            style="width: 147px;float:left;padding-top:10px;">
+
+                    </el-input>
+                    <div style="padding-left:0px;width: 1px;height: 60px;border-right: 1px solid black; float:left;"></div>
+
+                    <el-input
+                            v-model="namelist[i-1].next_ji"
+                            placeholder="输入晋升职级"
+                            size="large"
+                            style="width: 144px;float:left;padding-top:10px;">
+
+                    </el-input>
+                    <div style="padding-left:0px;width: 1px;height: 60px;border-right: 1px solid black; float:left;"></div>
+
+                    <div style="float:right;width:200px;margin-left: 10%;">
+                        <el-tooltip class="item" effect="light" content="添加一行" placement="left">
+                            <el-button type="text" @click="adddiv(i)" style="font-size: 30px;"><i
+                                    class="el-icon-circle-plus"></i>
+                            </el-button>
+                        </el-tooltip>
+
+                        <el-tooltip class="item" effect="light" content="删除本行" placement="right">
+                            <el-button type="text" @click="delectdiv(i)" style="font-size: 30px;"><i
+                                    class="el-icon-remove"></i>
+                            </el-button>
+                        </el-tooltip>
+
+
+                    </div>
+                </div>
+
+            </el-card>
+
+            <el-card class="box-card" style="width: 960px;margin: 2px auto;">
+                <el-input class="textarea"
+                          type="textarea"
+                          :rows="2"
+                          placeholder="请输入说明或备注"
+                          v-model="note"
+                          style="width: 100%;font-size: 16px;margin-top: 10px;">
                 </el-input>
             </el-card>
+
             <div style="text-align: right;width:80%;margin-left: 10%;margin-top: 10px;">
                 <el-tooltip class="item" effect="light" content="添加新组件" placement="top">
                     <el-button type="text" @click="adddiv" style="font-size: 30px;"><i class="el-icon-circle-plus"></i>
@@ -81,61 +119,78 @@
 </template>
 
 <script>
-    import muban3_Img from "@/assets/img/muban_3.png"
+    import tableData from "@/assets/data/table.json";
+
     export default {
         name: "mubanShow3",
-        data(){
-            return{
-                muban3_Img:muban3_Img,
-                department:'',
-                note:'',
-                date:'',
-                input_num:1,
+        data() {
+            return {
+                tableData3: tableData.table3,
+                department: '',
+                note: '',
+                date: '',
+                input_num: 1,
 
-                namelist:[
+                namelist: [
                     {
-                        xuhao:'',
-                        bumen:'',
-                        name:'',
-                        now_ji:'',
-                        next_ji:'',
+                        xuhao: '',
+                        bumen: '',
+                        name: '',
+                        now_ji: '',
+                        next_ji: '',
                     },
                 ]
 
             }
         },
-        methods:{
-            adddiv(){
+        mounted() {
+            let type = this.$store.state.ceping.ceping_type;
+            if (type == 3) {
+                this.input_num = this.$store.state.ceping.input_num;
+                this.note = this.$store.state.ceping.note;
+                this.department = this.$store.state.ceping.department;
+                this.date = this.$store.state.ceping.date;
+                this.namelist = this.$store.state.ceping.namelist;
+            }
+        },
+        methods: {
+            adddiv(i){
                 this.input_num+=1;
-                this.namelist.push({
-                    xuhao:'',
-                    bumen:'',
-                    name:'',
-                    now_ji:'',
-                    next_ji:'',
-                },)
+                this.namelist.splice(i,0,
+                    {
+                        xuhao: '',
+                        bumen: '',
+                        name: '',
+                        now_ji: '',
+                        next_ji: '',
+                    },
+                )
+                // console.log(JSON.stringify(this.namelist))
             },
-            delectdiv(){
+            delectdiv(i){
                 if(this.input_num>1){
                     this.input_num-=1;
-                    this.namelist.pop();
+                    this.namelist.splice(i-1,1);
                 }
+
+                // console.log(JSON.stringify(this.namelist))
             },
-            save_store(){
-                this.$store.commit('SET_CEPING_TYPE',3)
-                this.$store.commit('SET_NOTE',this.note)
-                this.$store.commit('SET_DEPARTMENT',this.department)
-                this.$store.commit('SET_DATE',this.date)
-                this.$store.commit('SET_NAMELIST',this.namelist)
+            save_store() {
+                this.$store.commit('SET_CEPING_TYPE', 3)
+                this.$store.commit('SET_NOTE', this.note)
+                this.$store.commit('SET_DEPARTMENT', this.department)
+                this.$store.commit('SET_DATE', this.date)
+                this.$store.commit('SET_NAMELIST', this.namelist)
+                this.$store.commit('SET_INPUT_NUM', this.input_num)
 
             },
 
-            submit(){
+            submit() {
                 this.save_store()
                 // console.log(JSON.stringify(this.namelist))
                 this.$router.push({
                     path: '/mubanManage/mubanShow/mubanShow0',
-                    query: {type:3}
+                    query: {type: 3}
                 })
 
 
@@ -145,8 +200,8 @@
     }
 </script>
 
-<style scoped>
-    .info_container{
+<style lang="less">
+    .info_container {
         padding: 20px;
         background: #fff;
         box-sizing: border-box;
@@ -154,6 +209,56 @@
         width: 1100px;
         margin: 0 auto;
         text-align: center;
+    }
+
+    .set_padding .el-card__body {
+        padding: 0px 20px;
+
+    }
+
+    .set_padding {
+        table {
+            border-collapse: collapse;
+        }
+
+        table, tr, td {
+            border: 1px solid black;
+        }
+
+        td {
+            width: 100px; /*这里需要自己调整，根据自己的需求调整宽度*/
+            height: 70px; /*这里需要自己调整，根据自己的需求调整高度*/
+            position: relative;
+        }
+
+        td[class=first]:before {
+            content: "";
+            position: absolute;
+            width: 1px;
+            height: 310px; /*这里需要自己调整，根据td的宽度和高度*/
+            top: 0;
+            left: 0;
+            background-color: black;
+            display: block;
+            transform: rotate(-63deg); /*这里需要自己调整，根据线的位置*/
+            transform-origin: top;
+        }
+    }
+
+    .card_style {
+        border-bottom: 1px solid black;
+        border-left: 1px solid black;
+        border-right: 1px solid black;
+        width: 918px;
+        min-height: 60px;
+    }
+
+    .el-card {
+        border: 1px solid #EBEEF5;
+        background-color: #FFF;
+        color: #303133;
+        -webkit-transition: .3s;
+        transition: .3s;
     }
 
 </style>
--- src/page/mubanManage/components/guding/mubanShow5.vue
+++ src/page/mubanManage/components/guding/mubanShow5.vue
@@ -2,17 +2,9 @@
     <div class="fillcontain" ref="fillcontain">
         <div class="info_container" ref="info_container">
             <h1>派遣人员考核民主测评票</h1>
-            <el-card class="box-card" style="width: 960px;margin: 5px auto;">
-                <el-input class="textarea"
-                          type="textarea"
-                          :rows="2"
-                          placeholder="请输入说明或备注"
-                          v-model="note"
-                          style="width: 100%;font-size: 16px;margin-top: 10px;">
-                </el-input>
-            </el-card>
-            <el-card class="box-card" style="width: 960px;margin: 5px auto;">
-                <div style="width: 960px;height: 20px;margin: 0 auto">
+
+            <el-card class="box-card" style="width: 960px;margin: 2px auto;">
+                <div style="width: 960px;height: 20px;padding-bottom: 10px;">
                     <span style="float: left">部门:<el-input v-model="department" placeholder="请输入部门" clearable style="width: 200px;"></el-input></span>
                     <span style="float:left;padding-left: 450px;" >
                     <div class="block">
@@ -29,21 +21,41 @@
                 </span>
                 </div>
             </el-card>
-            <el-card class="box-card" style="width:960px;margin: 1px auto;"><img style="width: 900px;height: 150px;padding-top: 10px;" :src="muban5_Img" /></el-card>
-            <el-card class="box-card" v-for="i in input_num" style="width: 960px;min-height: 60px;margin: 0px auto;">
-                <el-input v-model="namelist[i-1].name" placeholder="请输入被测评人姓名" size="large" clearable style="width: 300px;padding-left:10px;float:left;" ></el-input>
+            <el-card class="box-card set_padding"  style="width: 960px;margin: 0px auto;" >
+                <div v-html="tableData1" class="table_show"></div>
             </el-card>
-            <div style="text-align: right;width:80%;margin-left: 10%;margin-top: 10px;">
-                <el-tooltip class="item" effect="light" content="添加新组件" placement="top">
-                    <el-button type="text" @click="adddiv" style="font-size: 30px;"><i class="el-icon-circle-plus"></i>
-                    </el-button>
-                </el-tooltip>
+            <el-card class="box-card set_padding" v-for="i in input_num" style="width: 960px;min-height: 62px;margin: 0px auto;">
+                <div class="card_style">
+                    <el-input v-model="namelist[i-1].name" placeholder="请输入被测评人姓名" size="large" clearable style="width: 270px;float:left;padding-top:10px;" ></el-input>
+                    <div style="padding-left:7px;width: 1px;height: 60px;border-right: 1px solid black; float:left;"></div>
+                    <div style="float:right;width:200px;margin-left: 10%;">
+                        <el-tooltip class="item" effect="light" content="添加一行" placement="left">
+                            <el-button type="text" @click="adddiv(i)" style="font-size: 30px;"><i class="el-icon-circle-plus"></i>
+                            </el-button>
+                        </el-tooltip>
 
-                <el-tooltip class="item" effect="light" content="删除组件" placement="top">
-                    <el-button type="text" @click="delectdiv" style="font-size: 30px;"><i class="el-icon-remove"></i>
-                    </el-button>
-                </el-tooltip>
-            </div>
+                        <el-tooltip class="item" effect="light" content="删除本行" placement="right">
+                            <el-button type="text" @click="delectdiv(i)" style="font-size: 30px;"><i class="el-icon-remove"></i>
+                            </el-button>
+                        </el-tooltip>
+
+
+                    </div>
+                </div>
+
+            </el-card>
+
+
+
+            <el-card class="box-card" style="width: 960px;margin: 2px auto;">
+                <el-input class="textarea"
+                          type="textarea"
+                          :rows="2"
+                          placeholder="请输入说明或备注"
+                          v-model="note"
+                          style="width: 100%;font-size: 16px;margin-top: 10px;">
+                </el-input>
+            </el-card>
             <div style="text-align: center;padding-bottom: 20px;">
                 <el-button type="primary" @click="submit()">预览发布</el-button>
             </div>
@@ -52,36 +64,53 @@
 </template>
 
 <script>
-    import muban5_Img from "@/assets/img/muban_5.png"
+    import tableData from "@/assets/data/table.json";
     export default {
         name: "mubanShow5",
         data(){
             return{
                 note:'',
-                muban5_Img:muban5_Img,
+                tableData1:tableData.table5,
                 department:'',
                 date:'',
                 input_num:1,
+
                 namelist:[{
                     name:'',
                 },],
 
             }
         },
+        mounted(){
+            let type = this.$store.state.ceping.ceping_type;
+            if(type==5){
+                this.input_num = this.$store.state.ceping.input_num;
+                this.note = this.$store.state.ceping.note;
+                this.department = this.$store.state.ceping.department;
+                this.date = this.$store.state.ceping.date;
+                this.namelist = this.$store.state.ceping.namelist;
+            }
+        },
+
+
+
         methods:{
-            adddiv() {
-                this.input_num += 1;
-                this.namelist.push(
+            adddiv(i){
+                this.input_num+=1;
+                this.namelist.splice(i,0,
                     {
-                        name: '',
+                        name:'',
                     },
                 )
+                // console.log(JSON.stringify(this.namelist))
             },
-            delectdiv(){
+            delectdiv(i){
                 if(this.input_num>1){
                     this.input_num-=1;
-                    this.namelist.pop();
+                    this.namelist.splice(i-1,1);
                 }
+
+                // console.log(JSON.stringify(this.namelist))
             },
             save_store(){
                 this.$store.commit('SET_CEPING_TYPE',5)
@@ -89,6 +118,8 @@
                 this.$store.commit('SET_DEPARTMENT',this.department)
                 this.$store.commit('SET_DATE',this.date)
                 this.$store.commit('SET_NAMELIST',this.namelist)
+                this.$store.commit('SET_INPUT_NUM',this.input_num)
+
             },
             submit(){
                 this.save_store()
@@ -98,13 +129,13 @@
                 })
 
 
-            },
+            }
 
         }
     }
 </script>
 
-<style scoped>
+<style lang="less" >
     .info_container{
         padding: 20px;
         background: #fff;
@@ -114,5 +145,53 @@
         margin: 0 auto;
         text-align: center;
     }
+
+
+    .set_padding .el-card__body{
+        padding: 0px 20px;
+
+    }
+    .set_padding{
+        table{
+            border-collapse:collapse;
+        }
+        table,tr,td{
+            border:1px solid black;
+        }
+        td{
+            width:100px;/*这里需要自己调整，根据自己的需求调整宽度*/
+            height:70px;/*这里需要自己调整，根据自己的需求调整高度*/
+            position: relative;
+        }
+        td[class=first]:before{
+            content: "";
+            position: absolute;
+            width: 1px;
+            height:310px;/*这里需要自己调整，根据td的宽度和高度*/
+            top:0;
+            left:0;
+            background-color: black;
+            display: block;
+            transform: rotate(-63deg);/*这里需要自己调整，根据线的位置*/
+            transform-origin: top;
+        }
+    }
+    .card_style{
+        border-bottom: 1px solid black;
+        border-left: 1px solid black;
+        border-right: 1px solid black;
+        width: 918px;
+        min-height: 60px;
+    }
+    .el-card {
+        border: 1px solid #EBEEF5;
+        background-color: #FFF;
+        color: #303133;
+        -webkit-transition: .3s;
+        transition: .3s;
+    }
+
+
+
 
 </style>
